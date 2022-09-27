a9649c2197d6ff515155cc54b03875ab47382184
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-09-27 17:54:46 +0300

    Add play_hooky method

--- student.rb
+++ student.rb
@@ -1,2 +1,7 @@
+require "./person.rb"
 class Student < Person
+    def initialize(classroom)
+        super (name = "Unknown", age, parent_permission = true)
+        @classroom = classroom
+    end
 end
