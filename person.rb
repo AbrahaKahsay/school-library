11062ba481b36ad1d0dfabdabaafb1ee6d595772
AbrahaKahsay <abrahak81@gmail.com>
2022-09-27 16:26:05 +0300

    Add getter for id using attr_reader method

--- person.rb
+++ person.rb
@@ -1,5 +1,6 @@
 class Person
     attr_accessor :name, :age
+    attr_reader :id
     def initialize(name = "Unknown", age, parent_permission = true)
         @id = Random.rand(1..1000)
         @name = name
