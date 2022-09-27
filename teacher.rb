20932243e7afb9d6312b38b4f558c39c3ac2e151
AbrahaKahsay <abrahak81@gmail.com>
2022-09-27 18:03:47 +0300

    Override the can_use_services? to be always true

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,10 @@
 class Teacher < Person
     def initialize(specialization)
-        super(name = "Unknown", age, parent_permission = true)
+        super(name = "Unknown", age)
         @specialization = specialization
+    end
+
+    def can_use_services?
+        true
     end
 end
