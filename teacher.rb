109426b7e409075f70cc2e307b0f5e8964dd124c
AbrahaKahsay <abrahak81@gmail.com>
2022-09-27 18:47:27 +0300

    Fix linter errors

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,11 @@
+require './person'
 class Teacher < Person
-    def initialize(specialization)
-        super(name = "Unknown", age)
-        @specialization = specialization
-    end
+  def initialize(specialization)
+    super
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
