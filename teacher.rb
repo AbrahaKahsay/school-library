5e61472d8df2b4c9d6535e17f82d7f6437c07bb2
AbrahaKahsay <abrahak81@gmail.com>
2022-09-27 19:55:56 +0300

    Correct constructor for teacher class according to code reviewer

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require './person'
 class Teacher < Person
-  def initialize(specialization)
-    super
+  def initialize(age,specialization, name = "Unknown", parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
