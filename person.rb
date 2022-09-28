92e809a6f48b8815758736f7822b704bf2b4d62a
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-09-28 21:21:49 +0300

    Add correct_name method to Person class

--- person.rb
+++ person.rb
@@ -5,10 +5,15 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
