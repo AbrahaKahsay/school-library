845936594366fb93fa9786b572d811d943f5b32c
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-09-29 18:34:06 +0300

    Add classroom= method to update classrom in student

--- student.rb
+++ student.rb
@@ -1,5 +1,7 @@
 require './person'
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -7,5 +9,11 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.includes?(self)
+
   end
 end
