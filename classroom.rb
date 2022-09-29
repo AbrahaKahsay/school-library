f846941557680d4cf3bdcc288bd64419edf79263
AbrahaKahsay <abrahak81@gmail.com>
2022-09-29 19:10:17 +0300

    Fix linter errors and finish tasks

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,13 @@
 class Classroom
+  attr_accessor :label, :students
 
-    attr_accessor :label, :students
+  def initialize(label)
+    @label = label
+    @students = []
+  end
 
-    def initialize(label)
-        @label = label
-        @students = []
-    end
-
-    def add_student(student)
-        @students.push(student) unless students.includes?(student)
-        students.classroom = self
-    end
+  def add_student(student)
+    @students.push(student) unless students.includes?(student)
+    students.classroom = self
+  end
 end
