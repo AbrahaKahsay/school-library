dedc18350fd2c57c8b7cfb7c1a4d8d077d7b4d6c
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-09-29 18:25:43 +0300

    Add add_student method to classroom

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,14 @@
 class Classroom
-    attr_accessor :label
+
+    attr_accessor :label, :students
+
     def initialize(label)
         @label = label
+        @students = []
+    end
+
+    def add_student(student)
+        @students.push(student) unless students.includes?(student)
+        students.classroom = self
     end
 end
