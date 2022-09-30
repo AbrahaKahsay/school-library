8b07070da2cf6f2ba7d1615fce1467aab2924b8d
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-09-30 11:22:41 +0300

    Add list of people

--- app.rb
+++ app.rb
@@ -9,3 +9,9 @@ def list_books
     @books.each_with_index do |book, index|
     puts "#{index} Title: #{book.title}, Author: #{book.author}"
 end
+
+#list all people
+def list_persons
+    @persons.each_with_index do |person, index|
+    puts "#{index} Name: #{person.name}, Id: #{person.id}, Age: #{person.age}"
+end
