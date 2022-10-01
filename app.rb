d171488a27c4b274ac8bd7aecadf9e923eb840ec
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-10-01 11:48:02 +0300

    Add main.rb and Fix linter errors

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/MethodLength
 require './person'
 require './student'
 require './teacher'
@@ -7,98 +8,101 @@ require './rental'
 @books = []
 @persons = []
 @rentals = []
-@classroom = Classroom.new("CR 1001")
+@classroom = Classroom.new('CR 1001')
 
 # list all books
 def list_books
-    @books.each_with_index do |book, index|
+  @books.each_with_index do |book, index|
     puts "#{index} Title: #{book.title}, Author: #{book.author}"
+  end
 end
 
-#list all people
+# list all people
 def list_persons
-    @persons.each_with_index do |person, index|
+  @persons.each_with_index do |person, index|
     puts "#{index} Name: #{person.name}, Id: #{person.id}, Age: #{person.age}"
+  end
 end
 
 # Create a person (teacher or student, not a plain Person).
 def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
-    
-    input = gets.chop.to_i
-    if [1, 2].include?(input)
-        print 'Name: '
-        name = gets.chomp
-        print 'age: '
-        age = gets.chomp.to_i
-        case input
-        when 1
-            print 'Has parent permission [Y/N]: '
-            permission = gets.chomp
-            permission.upcase == 'Y'
-            student = Student.new(age, classroom, name, permission)
-            @persons.push(student)
-            puts 'Person created  successfully'
-        when 2
-            print 'Specialization: '
-            specialization = gets.chomp
-            teacher = Teacher.new(age, specialization, name)
-            @persons.push(teacher)
-            puts 'Person created  successfully'
-        end
-    else
-        puts 'Invalid Person'
+  print 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+
+  input = gets.chop.to_i
+  if [1, 2].include?(input)
+    print 'Name: '
+    name = gets.chomp
+    print 'age: '
+    age = gets.chomp.to_i
+    case input
+    when 1
+      print 'Has parent permission [Y/N]: '
+      permission = gets.chomp
+      permission.upcase = 'Y'
+      student = Student.new(age, classroom, name, permission)
+      @persons.push(student)
+      puts 'Person created  successfully'
+    when 2
+      print 'Specialization: '
+      specialization = gets.chomp
+      teacher = Teacher.new(age, specialization, name)
+      @persons.push(teacher)
+      puts 'Person created  successfully'
     end
+  else
+    puts 'Invalid Person'
+  end
 end
 
 # Create a book.
 def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    new_book = Book.new(title, author)
-    @books.push(new_book)
-    puts 'Book created successfuly'
+  print 'Title: '
+  title = gets.chomp
+  print 'Author: '
+  author = gets.chomp
+  new_book = Book.new(title, author)
+  @books.push(new_book)
+  puts 'Book created successfuly'
 end
 
 # Create a rental.
 def create_rental
-    puts 'Select a book from the following list by number'
+  puts 'Select a book from the following list by number'
 
-    list_books
-    book_id = gets.chomp
-    book = nil
-    book = @books[book_id.to_i] if book_id.to_i.to_s == book_id && book_id.to_i < @books.length
-    puts 'Select a person from the following list by number (not id)'
+  list_books
+  book_id = gets.chomp
+  book = nil
+  book = @books[book_id.to_i] if book_id.to_i.to_s == book_id && book_id.to_i < @books.length
+  puts 'Select a person from the following list by number (not id)'
 
-    list_persons
-    person = nil
-    person_id = gets.chomp
-    person = @persons[person_id.to_i] if person_id.to_i.to_s == person_id && person_id.to_i < @persons.length
-    if book && person
-      print 'Date: '
-      date = gets.chomp
-      new_rental = Rental.new(date, book, person)
-      @rentals.push(new_rental)
-      puts 'Rental created successfuly'
-    else
-      puts 'Invalid person or book ID'
-    end
+  list_persons
+  person = nil
+  person_id = gets.chomp
+  person = @persons[person_id.to_i] if person_id.to_i.to_s == person_id && person_id.to_i < @persons.length
+  if book && person
+    print 'Date: '
+    date = gets.chomp
+    new_rental = Rental.new(date, book, person)
+    @rentals.push(new_rental)
+    puts 'Rental created successfuly'
+  else
+    puts 'Invalid person or book ID'
+  end
 end
 
 # List all rentals for a given person id.
 def list_all_rentals
-    print 'ID of person: '
-    id = gets.chomp
-    person = @persons.filter { |val| val.id == id.to_i }
-    if person.length.positive?
-        person = person[0]
-        @rentals.each do |rent|
-            puts "Date: #{rent.date}, Book: #{rent.book.title} by #{rent.book.author}" unless rent.person.id != person.id
-        end
-    else
-      puts 'No person found with that ID'
+  print 'ID of person: '
+  id = gets.chomp
+  person = @persons.filter { |val| val.id == id.to_i }
+  if person.length.positive?
+    person = person[0]
+    @rentals.each do |rent|
+      puts "Date: #{rent.date}, Book: #{rent.book.title} by #{rent.book.author}" unless rent.person.id != person.id
     end
-    ''
+  else
+    puts 'No person found with that ID'
+  end
+  ''
 end
+# rubocop:enable Metrics/MethodLength
---
+++ main.rb
@@ -0,0 +1,38 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+require './app'
+def main # rubocop:disable Metrics/MethodLength
+  menu = [
+    "\n\nPlease choose an option by entering a bunber",
+    '1 - List all books',
+    '2 - List all people',
+    '3 - create a person',
+    '4 - create a book',
+    '5 - create a rental',
+    '6 - List all rental for a given person id',
+    "7 - Exit \n\n"
+  ]
+  puts menu
+
+  input = gets.chomp.to_i
+  while input != 7
+    case input
+    when 1
+      list_books
+    when 2
+      list_persons
+    when 3
+      puts create_person
+    when 4
+      puts create_book
+    when 5
+      puts create_rental
+    when 6
+      puts list_all_rentals
+    end
+    puts menu
+    input = gets.chomp.to_i
+  end
+end
+
+main
+# rubocop:enable Metrics/CyclomaticComplexity
